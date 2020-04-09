7102b3cd2e0649f1481d0761bf8cb69b329b438d
Julien Vollering <julienvollering@gmail.com>
2020-04-09 22:44:49 +0200

    Remove polygon-intersecting tally. Speed up DTM extraction. Adjust for M geometry in source polygons.

--- R/locality-functions.R
+++ R/locality-functions.R
@@ -144,17 +144,11 @@ add_seeds_WALD <- function(grid, raster) {
 
 add_relative_elevation <- function(grid, sourcepoly, dtm) {
   
-  sourcepoly <- as(sourcepoly, "Spatial") %>% 
-    sp::spTransform(CRSobj = raster::projection(dtm))
-  elevref <- raster::extract(dtm, sourcepoly, fun = max) %>% 
-    as.numeric() %>% 
-    max()
+  sourcepoly <- sf::st_transform(sourcepoly, crs = raster::projection(dtm))
+  elevref <- exactextractr::exact_extract(dtm, sourcepoly, fun = 'max')
   
-  grd.proj <- grid %>% 
-    as("Spatial") %>%   
-    sp::spTransform(CRSobj = raster::projection(dtm))
-  elev <- raster::extract(dtm, grd.proj, fun = mean) %>% 
-    as.numeric
+  grd.proj <- sf::st_transform(grid, crs = raster::projection(dtm)) 
+  elev <- exactextractr::exact_extract(dtm, grd.proj, fun = 'mean', progress = FALSE)
   grid %>% 
     add_column(relelev = elev, .before = "geometry") %>% 
     mutate(relelev = relelev - elevref)
